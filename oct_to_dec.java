b144c49c25959616fd550afb01c01231b1975574
Mahendiran T <77065963+Mahendiran15798@users.noreply.github.com>
2021-01-08 07:03:18 +0530

    Add files via upload

---
+++ oct_to_dec.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class oct_to_dec {
+ public static void main(String [] args){
+     long octal_num, decimal_num =0;
+     int i=0;
+     Scanner in =new Scanner(System.in);
+     System.out.println("Enter any octal number : ");
+     octal_num =in.nextLong();
+
+
+ }
+}
