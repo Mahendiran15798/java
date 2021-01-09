68ba59f1b10ee5ebdba2e68a7b7b451baaeccafc
Mahendiran T <77065963+Mahendiran15798@users.noreply.github.com>
2021-01-09 16:51:49 +0530

    Add files via upload

---
+++ add_or_even.java
@@ -0,0 +1,13 @@
+
+public class add_or_even {
+	public static void main(String args[]) {
+		int no=17;
+		if (no==0)
+			System.out.println("Nothing");
+		else if (no%2==0)
+			System.out.println("odd");
+		else
+			System.out.println("even");
+	}
+
+}
