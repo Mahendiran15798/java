7ed1370fc4cfe9a40f5b20e06ca609ee2fca9d6f
Mahendiran T <77065963+Mahendiran15798@users.noreply.github.com>
2021-01-09 13:56:07 +0530

    Add files via upload

---
+++ ArithmaticOperator.java
@@ -0,0 +1,20 @@
+
+public class ArithmaticOperator {
+	public static void main(String args[]) {
+		
+		int number1=12,number2=22;
+		int no=number1+number2;
+		int no1=number1-number2;
+		int no2=number1*number2;
+		int no3=number1/number2;
+		int no4=number1%number2;
+		
+		System.out.println(no);
+		System.out.println(no1);
+		System.out.println(no2);
+		System.out.println(no3);
+		System.out.println(no4);
+		
+	}
+
+}
